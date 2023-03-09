28ad587c72169dd686ee3db7a685f8cddea118f8
MickMarch <michael.r.marchand@gmail.com>
2023-03-08 21:11:05 -0500

    Added some GeoJSON functionality

--- mapping_geojson_points/static/js/logic.js
+++ mapping_geojson_points/static/js/logic.js
@@ -1,71 +1,49 @@
 // Import any dependencies
 var cityData = cities
 
-// Add console.log to check to see if our code is working.
-console.log("working");
-
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport = {
-    "type": "FeatureCollection",
-    "features": [{
-        "type": "Feature",
-        "properties": {
-            "id": "3469",
-            "name": "San Francisco International Airport",
-            "city": "San Francisco",
-            "country": "United States",
-            "faa": "SFO",
-            "icao": "KSFO",
-            "alt": "14",
-            "tz-offset": "-8",
-            "dst": "A",
-            "tz": "America/Los Angeles"
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [-122.375, 37.61899948120117]
-        }
-    }]
-};
-
-// --------------------------
-// Grabbing our GeoJSON Data
-// --------------------------
-
-// L.geoJSON(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     pointToLayer: function (feature, latlng) {
-//         console.log(feature);
-//         return L.marker(latlng)
-//             .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
-//     }
-// }).addTo(map);
-
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function (feature, layer) {
-        console.log(layer);
-        layer.bindPopup("<h2>Airport Code: " + layer.feature.properties.faa + "</h2> <hr> <h3>Airport Name: " + layer.feature.properties.name + "</h3>");
-    }
-}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    // Alternate Styling vvvv
-    // id: 'mapbox/streets-v11',
-    id: 'mapbox/navigation-night-v1',
-    // id: 'mapbox/dark-v11',
-    // id: 'mapbox/satellite-streets-v11',
-    // id: 'mapbox/light-v11',
+    id: 'mapbox/streets-v11',
+    accessToken: API_KEY
+});
+// We create the tile layer that will be the background of our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v11',
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// Create a base layer for the maps
+let baseMaps = {
+    "Street View": streets,
+    "Dark View": dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Accessing airport data through JSON URL
+let airportData = "https://raw.githubusercontent.com/MickMarch/mapping_earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data
+d3.json(airportData).then(function (data) {
+    console.log(data);
+    // Creating a GeoJSON Layer with the retrieved data
+    L.geoJSON(data, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("<h2>Airport Code: " + layer.feature.properties.faa + "</h2> <hr> <h2>Airport Name: " +
+                layer.feature.properties.name + "</h2>")
+        }
+    }).addTo(map)
+});
 
 
-
+L.control.layers(baseMaps).addTo(map);
